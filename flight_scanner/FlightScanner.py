83a8db89930154261759f8164365a81a10fc3222
Geri Peikova <gergana.peikova@gmail.com>
2023-10-08 16:09:36 +0300

    accept cookies and input Rome to textbox

--- flight_scanner/FlightScanner.py
+++ flight_scanner/FlightScanner.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def scanning():
@@ -8,5 +11,15 @@ def scanning():
     driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     driver.get('https://www.google.com/travel/flights')
+    button_accept_all = driver.find_element(By.XPATH, '/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button')
+    wait = WebDriverWait(driver, timeout=2)
+    wait.until(lambda d: button_accept_all.is_displayed())
+    button_accept_all.click()
 
-    
+    driver.find_element(By.XPATH,
+                        '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input') \
+        .send_keys('Rome')
+
+    print(dir(driver))
+    my_select = Select(driver.find_element(By.XPATH('/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul')))
+    my_select.select_by_index(1)
