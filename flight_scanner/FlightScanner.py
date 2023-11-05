98ee9aff81f8f62616bbb6e3f097729a07b96f3d
Geri Peikova <gergana.peikova@gmail.com>
2023-11-05 14:40:30 +0200

    Sofia to Rome 10th of Nov - 12th of Nov

--- flight_scanner/FlightScanner.py
+++ flight_scanner/FlightScanner.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 #AIzaSyDwluc0SlxuEyjmn173yjSwlYe8Lcfe26o
 
@@ -28,28 +28,19 @@ def scanning():
         wait.until(lambda d: button_accept_all.is_displayed())
         button_accept_all.click()
     finally:
-
-        depart_input = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/input')
+        # WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//img[contains(@src, 'https://play-lh.googleusercontent.com/a-/')]/../following-sibling::div/div[@jscontroller]/span")))
+        depart_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/input')))
         wait.until(lambda d: depart_input.is_displayed())
         depart_input.send_keys("10 November")
         depart_input.send_keys(Keys.ENTER)
 
-        return_input = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[2]/div/input')
+        return_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[2]/div/input')))
         wait.until(lambda d: return_input.is_displayed())
         return_input.send_keys("12 November")
         return_input.send_keys(Keys.ENTER)
 
-        destination = driver.find_element(By.XPATH, '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input')
-        destination.click()
-        time.sleep(2)
-        destination = driver.find_element((By.CLASS_NAME, 'II2One j0Ppje zmMKJ LbIaRd'))
-        print(destination.text)
-        wait.until(lambda d: destination.is_displayed())
+        destination = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input')))
         destination.send_keys("Rome")
-
-
-        button_search = driver.find_element(By.XPATH,'/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/button')
-        print(button_search.text)
-
-        # <input type="text" value="" class="II2One j0Ppje zmMKJ LbIaRd" aria-label="Where else?"
-        # <input type="text" value="" jsname="yrriRe" jsaction="focus:h06R8; blur:zjh6rb" class="II2One j0Ppje zmMKJ LbIaRd" autocomplete="off" role="combobox" aria-autocomplete="both" aria-haspopup="true" aria-expanded="true" aria-label="Where else?" aria-describedby="i29" autofocus="" aria-owns="h0T7hb-158" tabindex="0">
+        destination = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,
+                                                                                        '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[3]/ul/li[1]')))
+        destination.click()
