4296fc1797e5841973cb6eb3de147f5977d0fe27
ellektrria <gergana.peikova@gmail.com>
2024-04-06 00:34:57 +0300

    Fix sizes and add loading pic

--- flight_scanner/MyMenuWindow.py
+++ flight_scanner/MyMenuWindow.py
@@ -1,12 +1,10 @@
-from datetime import datetime, timedelta
+import time
 
-from PyQt5.uic.properties import QtWidgets
-from dateutil.relativedelta import relativedelta
-
+from PyQt5.QtGui import QPixmap, QMovie
 from PyQt5.QtWidgets import QWidget, QLabel, QComboBox, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton, \
-    QMessageBox, QListWidget, QListWidgetItem, QGridLayout, QGraphicsDropShadowEffect
+    QMessageBox, QGridLayout
 
-from date_utils import format_datetime_to_textdate_and_time
+from date_utils import format_datetime_to_textdate_and_time, generate_dates
 from flight_scanner import scanning, get_sorted_list_flights
 from interpreters import driver_setup
 from tests.setup import LIST_FLIGHTS_UNSORTED
@@ -19,25 +17,26 @@ class MyMenuWindow(QWidget):
         self.init_ui()
 
     def init_ui(self):
+
         # Create dropdown menus for days of the week
-        departure_weekday_label = QLabel('Select Departure Day:')
+        self.departure_weekday_label = QLabel('Select Departure Day:')
         self.week_days_combobox1 = QComboBox(self)
         self.week_days_combobox1.addItems(
             ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
         self.week_days_combobox1.setPlaceholderText('Week Days')
 
-        arrival_weekday_label = QLabel('Select Arrival Day:')
+        self.arrival_weekday_label = QLabel('Select Arrival Day:')
         self.week_days_combobox2 = QComboBox(self)
         self.week_days_combobox2.addItems(
             ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
         self.week_days_combobox2.setPlaceholderText('Week Days')
 
         # Create textboxes with default and hint text
-        from_label = QLabel('From:')
+        self.from_label = QLabel('From:')
         self.from_textbox = QLineEdit('Sofia', self)
         self.from_textbox.setPlaceholderText('From where?')
 
-        to_label = QLabel('To:')
+        self.to_label = QLabel('To:')
         self.to_textbox = QLineEdit(self)
         self.to_textbox.setPlaceholderText('Where to?')
 
@@ -48,14 +47,14 @@ class MyMenuWindow(QWidget):
         day_layout = QHBoxLayout()
         text_layout = QHBoxLayout()
 
-        day_layout.addWidget(departure_weekday_label)
+        day_layout.addWidget(self.departure_weekday_label)
         day_layout.addWidget(self.week_days_combobox1)
-        day_layout.addWidget(arrival_weekday_label)
+        day_layout.addWidget(self.arrival_weekday_label)
         day_layout.addWidget(self.week_days_combobox2)
 
-        text_layout.addWidget(from_label)
+        text_layout.addWidget(self.from_label)
         text_layout.addWidget(self.from_textbox)
-        text_layout.addWidget(to_label)
+        text_layout.addWidget(self.to_label)
         text_layout.addWidget(self.to_textbox)
 
         layout.addLayout(day_layout)
@@ -63,11 +62,16 @@ class MyMenuWindow(QWidget):
         layout.addWidget(self.button_look_for_flights)
 
         self.button_look_for_flights.clicked.connect(self.on_clickable_button_click)
-        self.setLayout(layout)
 
+        self.label_loading = QLabel(self)
+        layout.addWidget(self.label_loading)
+
+        self.setFixedSize(1500, 450)
         self.setGeometry(800, 400, 400, 200)
         self.setWindowTitle('Day and Textbox Selector')
         self.show()
+
+        self.setLayout(layout)
 
     def on_clickable_button_click(self):
         input_data = {'departure_weekday': self.week_days_combobox1.currentText(),
@@ -75,7 +79,13 @@ class MyMenuWindow(QWidget):
 
         if (not bool(self.from_textbox.text())) or (not bool(self.to_textbox.text())):
             info_box('Information', 'Fill in all details before proceeding!')
+
         else:
+            widgets = [self.departure_weekday_label, self.week_days_combobox1, self.arrival_weekday_label,
+                       self.week_days_combobox2, self.from_textbox, self.to_textbox, self.button_look_for_flights,
+                       self.from_label, self.to_label]
+            start_loading(self.label_loading, widgets)
+            time.sleep(5)
             input_data['flight_from'] = self.from_textbox.text()
             input_data['flight_to'] = self.to_textbox.text()
             print('\nFlight data: ', input_data)
@@ -93,6 +103,15 @@ class MyMenuWindow(QWidget):
             self.hide()
 
 
+def start_loading(label_loading, widgets):
+    movie = QMovie("loading.gif")
+    label_loading.setMovie(movie)
+    movie.start()
+    label_loading.show()
+    for widget in widgets:
+        widget.hide()
+
+
 def info_box(title, text):
     info_box = QMessageBox()
     info_box.setIcon(QMessageBox.Information)
@@ -100,37 +119,6 @@ def info_box(title, text):
     info_box.setText(text)
     info_box.setStandardButtons(QMessageBox.Ok)
     info_box.exec()
-
-
-def generate_dates(start_day, end_day, num_months=1):
-    start_day = get_index_by_weekday_name(start_day)
-    end_day = get_index_by_weekday_name(end_day)
-    dates_list = []
-    today = datetime.now()
-
-    # Find the next Friday
-    current_day_of_week = today.weekday()
-    days_until_start_day = (start_day - current_day_of_week + 7) % 7
-    next_weekday = today + timedelta(days=days_until_start_day)
-
-    # Print all the dates starting from Friday and ending on Monday for the next two months
-    while next_weekday + timedelta(days=end_day) <= today + relativedelta(
-            months=+num_months):  # Print for the next two months (8 weeks)
-        if start_day >= end_day:
-            week_set = {
-                'End': (next_weekday + timedelta(days=end_day) - timedelta(start_day - 7)).strftime('%a, %b %d')}
-        else:
-            week_set = {'End': (next_weekday + timedelta(days=end_day)).strftime('%a, %b %d')}
-        week_set['Start'] = next_weekday.strftime('%a, %b %d')
-        dates_list.append(week_set)
-        next_weekday += timedelta(days=7)
-    return dates_list
-
-
-def get_index_by_weekday_name(weekday):
-    for i, day in enumerate(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']):
-        if day == weekday:
-            return i
 
 
 def open_link(url):
@@ -145,9 +133,10 @@ class ScannedFlightsWindow(QWidget):
 
         layout = QGridLayout()
         self.setLayout(layout)
-        self.setFixedSize(800, 300)
+        self.setFixedSize(1500, 450)
         self.setGeometry(800, 400, 400, 200)
         self.setWindowTitle('Flights List')
+        self.setStyleSheet('''background: #EBF9FE''')
 
         self.label_round_trip = QLabel(f"Round trip: {input_data['flight_from']} - {input_data['flight_to']}")
         self.label_round_trip.setStyleSheet(
@@ -161,13 +150,18 @@ class ScannedFlightsWindow(QWidget):
         buttons = []
         for i, flight in enumerate(scanned_result):
             label_price = QLabel(f"| Price: {flight.price} |")
-            label_price.setStyleSheet('background-color: rgba(173, 216, 230, 0.5); border: 1px solid rgba(0, 0, 255, 0.5); padding: 5px; border-radius: 5px;')
+            label_price.setStyleSheet(
+                'background-color: rgba(173, 216, 230, 0.5); border: 1px solid rgba(0, 0, 255, 0.5); padding: 5px; border-radius: 5px;')
             layout.addWidget(label_price, i + 1, 0)
-            label_departure = QLabel(f"| Departure: {format_datetime_to_textdate_and_time(flight.departure_flight.departure_time)} |")
-            label_departure.setStyleSheet('background-color: rgba(173, 216, 230, 0.5); border: 1px solid rgba(0, 0, 255, 0.5); padding: 5px; border-radius: 5px;')
+            label_departure = QLabel(
+                f"| Departure: {format_datetime_to_textdate_and_time(flight.departure_flight.departure_time)} |")
+            label_departure.setStyleSheet(
+                'background-color: rgba(173, 216, 230, 0.5); border: 1px solid rgba(0, 0, 255, 0.5); padding: 5px; border-radius: 5px;')
             layout.addWidget(label_departure, i + 1, 1)
-            label_return = QLabel(f"| Return: {format_datetime_to_textdate_and_time(flight.arrival_flight.arrival_time)} |")
-            label_return.setStyleSheet('background-color: rgba(173, 216, 230, 0.5); border: 1px solid rgba(0, 0, 255, 0.5); padding: 5px; border-radius: 5px;')
+            label_return = QLabel(
+                f"| Return: {format_datetime_to_textdate_and_time(flight.arrival_flight.arrival_time)} |")
+            label_return.setStyleSheet(
+                'background-color: rgba(173, 216, 230, 0.5); border: 1px solid rgba(0, 0, 255, 0.5); padding: 5px; border-radius: 5px;')
             layout.addWidget(label_return, i + 1, 2)
             button = QPushButton("More Data")
             button.setStyleSheet(
--- flight_scanner/date_utils.py
+++ flight_scanner/date_utils.py
@@ -1,4 +1,37 @@
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
+
+from dateutil.relativedelta import relativedelta
+
+
+def generate_dates(start_day, end_day, num_months=1):
+    start_day = get_index_by_weekday_name(start_day)
+    end_day = get_index_by_weekday_name(end_day)
+    dates_list = []
+    today = datetime.now()
+
+    # Find the next Friday
+    current_day_of_week = today.weekday()
+    days_until_start_day = (start_day - current_day_of_week + 7) % 7
+    next_weekday = today + timedelta(days=days_until_start_day)
+
+    # Print all the dates starting from Friday and ending on Monday for the next two months
+    while next_weekday + timedelta(days=end_day) <= today + relativedelta(
+            months=+num_months):  # Print for the next two months (8 weeks)
+        if start_day >= end_day:
+            week_set = {
+                'End': (next_weekday + timedelta(days=end_day) - timedelta(start_day - 7)).strftime('%a, %b %d')}
+        else:
+            week_set = {'End': (next_weekday + timedelta(days=end_day)).strftime('%a, %b %d')}
+        week_set['Start'] = next_weekday.strftime('%a, %b %d')
+        dates_list.append(week_set)
+        next_weekday += timedelta(days=7)
+    return dates_list
+
+
+def get_index_by_weekday_name(weekday):
+    for i, day in enumerate(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']):
+        if day == weekday:
+            return i
 
 
 def get_calendar_dates():
@@ -30,7 +63,3 @@ def get_calendar_dates():
 
 def format_datetime_to_textdate_and_time(dt):
     return dt.strftime("%d %b %Y, %H:%Mh")
-
-
-if __name__ == '__main__':
-    get_calendar_dates()
