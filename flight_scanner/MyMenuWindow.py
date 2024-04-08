ceca94ece2a8416f2c44d7564dea933bff919875
ellektrria <gergana.peikova@gmail.com>
2024-04-08 13:25:07 +0300

    Fix sizes

--- flight_scanner/MyMenuWindow.py
+++ flight_scanner/MyMenuWindow.py
@@ -1,6 +1,6 @@
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QWidget, QLabel, QComboBox, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton, \
-    QMessageBox, QGridLayout
+    QMessageBox, QGridLayout, QApplication
 from PyQt5 import QtCore
 
 from date_utils import format_datetime_to_textdate_and_time, generate_dates
@@ -17,36 +17,47 @@ class MyMenuWindow(QWidget):
         self.init_ui()
 
     def init_ui(self):
+        screen_size = QApplication.primaryScreen().size()
+        font_size = int(screen_size.height()/150)
+        self.setFixedSize(int(screen_size.width() / 2), int(screen_size.height() / 2))
+        self.setWindowTitle('Flight Scanner')
+        self.show()
 
         # Create dropdown menus for days of the week
         self.departure_weekday_label = QLabel('Select Departure Day:')
+        self.departure_weekday_label.setFont(QFont('Arial', font_size))
         self.week_days_combobox1 = QComboBox(self)
+        self.week_days_combobox1.setFont(QFont('Arial', font_size))
         self.week_days_combobox1.addItems(
             ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
         self.week_days_combobox1.setPlaceholderText('Week Days')
 
         self.arrival_weekday_label = QLabel('Select Arrival Day:')
+        self.arrival_weekday_label.setFont(QFont('Arial', font_size))
         self.week_days_combobox2 = QComboBox(self)
+        self.week_days_combobox2.setFont(QFont('Arial', font_size))
         self.week_days_combobox2.addItems(
             ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
         self.week_days_combobox2.setPlaceholderText('Week Days')
 
         # Create textboxes with default and hint text
         self.from_label = QLabel('From:')
+        self.from_label.setFont(QFont('Arial', font_size))
         self.from_textbox = QLineEdit('Sofia', self)
+        self.from_textbox.setFont(QFont('Arial', font_size))
         self.from_textbox.setPlaceholderText('From where?')
 
         self.to_label = QLabel('To:')
+        self.to_label.setFont(QFont('Arial', font_size))
         self.to_textbox = QLineEdit(self)
+        self.to_textbox.setFont(QFont('Arial', font_size))
         self.to_textbox.setPlaceholderText('Where to?')
 
         self.button_look_for_flights = QPushButton("Look for flights")
+        self.button_look_for_flights.setFont(QFont('Arial', font_size))
 
         self.label_loading = QLabel('')
-        font = QFont()
-        font.setFamily("Arial")
-        font.setPointSize(30)
-        self.label_loading.setFont(font)
+        self.label_loading.setFont(QFont('Arial', font_size*2))
         self.label_loading.setAlignment(QtCore.Qt.AlignCenter)
 
         self.loading_thread = None
@@ -75,11 +86,6 @@ class MyMenuWindow(QWidget):
         layout.addLayout(layout3)
 
         self.button_look_for_flights.clicked.connect(self.on_clickable_button_click)
-
-        self.setFixedSize(1500, 450)
-        self.setGeometry(800, 400, 400, 200)
-        self.setWindowTitle('Flight Scanner')
-        self.show()
 
         self.setLayout(layout)
 
@@ -130,44 +136,42 @@ class ScannedFlightsWindow(QWidget):
     def __init__(self, scanned_result, input_data):
         super().__init__()
 
+        screen_size = QApplication.primaryScreen().size()
+        font_size = int(screen_size.height() / 500)
+        self.setFixedSize(int(screen_size.width() / 2), int(screen_size.height() / 2))
+
         layout = QGridLayout()
         self.setLayout(layout)
-        self.setFixedSize(1500, 450)
-        self.setGeometry(800, 400, 400, 200)
         self.setWindowTitle('Cheapest flights List')
         self.setStyleSheet('''background: #EBF9FE''')
 
         self.label_round_trip = QLabel(f"Round trip: {input_data['flight_from']} - {input_data['flight_to']}")
-        self.label_round_trip.setStyleSheet(
-            '''
-                    font-family: "Brush Script MT", "Lucida Console", serif;
-                    font-size: 20px;
-                    color: black;
-                
-            ''')
+        self.label_round_trip.setFont(QFont('Brush Script MT', font_size * 2))
         layout.addWidget(self.label_round_trip, 0, 0)
         buttons = []
         for i, flight in enumerate(scanned_result):
             label_price = QLabel(f"| Price: {flight.price} |")
+            label_price.setFont(QFont('Brush Script MT', font_size * 2))
             label_price.setStyleSheet(
                 'background-color: rgba(173, 216, 230, 0.5); border: 1px solid rgba(0, 0, 255, 0.5); padding: 5px; border-radius: 5px;')
             layout.addWidget(label_price, i + 1, 0)
             label_departure = QLabel(
                 f"| Departure: {format_datetime_to_textdate_and_time(flight.departure_flight.departure_time)} |")
+            label_departure.setFont(QFont('Brush Script MT', font_size * 2))
             label_departure.setStyleSheet(
                 'background-color: rgba(173, 216, 230, 0.5); border: 1px solid rgba(0, 0, 255, 0.5); padding: 5px; border-radius: 5px;')
             layout.addWidget(label_departure, i + 1, 1)
             label_return = QLabel(
                 f"| Return: {format_datetime_to_textdate_and_time(flight.arrival_flight.arrival_time)} |")
+            label_return.setFont(QFont('Brush Script MT', font_size * 2))
             label_return.setStyleSheet(
                 'background-color: rgba(173, 216, 230, 0.5); border: 1px solid rgba(0, 0, 255, 0.5); padding: 5px; border-radius: 5px;')
             layout.addWidget(label_return, i + 1, 2)
             button = QPushButton("More Data")
+            button.setFont(QFont('Brush Script MT', font_size * 2))
             button.setStyleSheet(
                 '''
                 QPushButton {
-                    font-family: "Brush Script MT", "Lucida Console", serif;
-                    font-size: 15px;
                     background-color: #3498db;
                     color: white;
                     border-style: outset;
